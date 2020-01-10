3e48996b9ce96b3f76a3062c93eb5c5e5ca3720b
Madugula Venkata <126427@cognizant.com>
2020-01-09 18:20:28 -0600

    CHnages for Manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,22 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
+
 
 @RestController
 public class WelcomeController {
 
+    @Value("${welcome.message}")
+    private String message;
+
+    @Required
+    public void welcomeController(String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
